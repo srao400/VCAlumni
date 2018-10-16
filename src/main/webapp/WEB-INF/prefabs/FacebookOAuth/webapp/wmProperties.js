a3849a81d0c3d70f23e3ab8fc6e40c514f0ff53e
Srinivas Rao <srao@rubaya.io>
2018-10-16 20:59:18 +0000

    commit xx New files added - [FacebookOAuth,GoogleOAuth] Files modified - [profiles,webapp,pom.xml,app.properties]

---
+++ src/main/webapp/WEB-INF/prefabs/FacebookOAuth/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "default",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "FacebookOAuth",
+  "homePage" : "Main",
+  "name" : "FacebookOAuth",
+  "platformType" : "DEFAULT",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "PREFAB",
+  "version" : "2.3"
+};
---
+++ src/main/webapp/WEB-INF/prefabs/GoogleOAuth/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "default",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "GoogleOAuth",
+  "homePage" : "Main",
+  "name" : "GoogleOAuth",
+  "platformType" : "DEFAULT",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "PREFAB",
+  "version" : "2.1"
+};
