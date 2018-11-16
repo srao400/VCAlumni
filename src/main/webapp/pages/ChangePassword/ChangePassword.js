64154647b7684aacb3cbd24e18746c48e3e9274a
Srinivas Rao <srao@rubaya.io>
2018-11-16 02:27:49 +0000

    Fixed issues with Android details screen New files added - [icons,VCSS480800.png,vcss800480.png,vcss320200.png,vcss480320.png,vcss1280720.png,vcss1600960.png,VCSS200320.png,VCSS320480.png,VCSS7201280.png,VCSS9601600.png] Files modified - [ChangePassword,EventDetails,VCADB_users,securityService,project-security.xml,config.xml]

--- src/main/webapp/pages/ChangePassword/ChangePassword.js
+++ src/main/webapp/pages/ChangePassword/ChangePassword.js
@@ -27,8 +27,22 @@ Application.$controller("ChangePasswordPageController", ["$scope", function($sco
 
 
     $scope.button1Tap = function($event, $isolateScope) {
-        $scope.Variables.MD5ChangePassword.invoke();
-        $data.password = $scope.Variables.MD5ChangePassword.dataSet.value;
+        // $scope.Variables.MD5ChangePassword.invoke();
+        // $data.password = $scope.Variables.MD5ChangePassword.dataSet.value;
+        $scope.Variables.UpdatePWD.setInput("email", $scope.Widgets.text1.datavalue);
+        $scope.Variables.UpdatePWD.setInput("newpassword", $scope.Variables.MD5Encryptpassword.dataSet.value);
+        $scope.Variables.UpdatePWD.update();
+    };
+
+
+    $scope.oldPasswordBlur = function($event, $isolateScope) {
+        $scope.Widgets.label7.caption = $scope.Variables.getOldPassword_var.dataSet[$i].password;
+        if ($scope.Widgets.oldPassword.datavalue === $scope.Variables.getOldPassword_var.dataSet[$i].password) {
+            $scope.Widgets.label4_1.caption = "Password must contain at least 1 uppercase and lowercase alphabet and 1 number";
+        } else {
+            $scope.Widgets.label4_1.caption = "Error Occured - Old password is incorrect";
+        }
+
     };
 
 }]);
